32ff512691f036f62c5107c8caafaa0a855f71e4
Tharindu Hasthika <tharindubathigama@gmail.com>
2025-09-21 11:50:15 +0900

    chore: remove console log

--- src/core/InputManager.ts
+++ src/core/InputManager.ts
@@ -107,7 +107,6 @@ export class InputManager {
   }
 
   private triggerEvent(target: string, eventType: string, event?: Event): void {
-    console.log(`Triggering event: ${target} - ${eventType}`);
     const targetEvents = this.globalEvents.get(target);
     if (targetEvents && targetEvents.has(eventType)) {
       const callbacks = targetEvents.get(eventType)!;
